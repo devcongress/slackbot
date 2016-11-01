bb07795bf01ef4941d5225c886c8f47eb514d162
David Oddoye <oddoyedavid@gmail.com>
2016-11-01 12:52:59 +0000

    Added greetings to own file

---
+++ jobs/greeting.js
@@ -0,0 +1,16 @@
+/**
+ * Greeting from bot
+ * @author Andrew Smith <a.smith@silentworks.co.uk>
+ */
+module.exports = (channelId, iconUrl, bot, message = 'Good morning') => {
+  return () => {
+    console.log(new Date() + ' - ' + message);
+    bot.say({
+      channel: channelId,
+      text: message,
+      username: bot.identity.name,
+      icon_url: iconUrl,
+      as_user: false
+    });
+  }
+};
