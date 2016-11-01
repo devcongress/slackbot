ac623b38672880633ff5c66bc4cccd89a42c90e0
David Oddoye <oddoyedavid@gmail.com>
2016-11-01 12:48:26 +0000

    Refactored to load env variables or use default

--- config.js
+++ config.js
@@ -2,4 +2,10 @@ module.exports = {
   ATTACHMENT_COLOR: "#4abfa4",
   APOD_API_URL: `https://api.nasa.gov/planetary/apod?api_key=${process.env.APOD_API_KEY}`,
   APOD_WEB_URL: 'https://apod.nasa.gov/apod/astropix.html',
+  XE_WEB_URL: 'http://www.xe.com/currencyconverter/convert',
+  APP_NAME: process.env.appName || 'MyApp',
+  GENERAL_ID: process.env.generalId || '',
+  RANDOM_ID: process.env.randomId || '',
+  ICON_URL: process.env.iconUrl || '',
+  DICTIONARY_API_BASE_URL: 'http://www.dictionaryapi.com/api/v1/references/collegiate/xml'
 }
