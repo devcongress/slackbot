61c8c64a141c87258b2e2623d0fdc71419b1a930
paul karikari <paulkarikari1@gmail.com>
2016-12-11 20:27:43 +0000

    added daily motivational message for devcongress slack group

--- jobs/index.js
+++ jobs/index.js
@@ -2,15 +2,18 @@
 
 
 const greetingCommand = require('./greeting');
+const getMotivationalMessage = require('./getMotivationalMessage');
 const config = require('../config');
 
 
 module.exports = (scheduler, bot) => {
   const goodMorningGreetingCommand = greetingCommand(config.GENERAL_ID, bot);
+  const getMotivationalMessageCommand = getMotivationalMessage(config.GENERAL_ID, bot);
   const goodNightGreetingCommand = greetingCommand(config.GENERAL_ID, bot, `Good night ${config.APP_NAME}`);
   const getAstronomyPictureOfTheDayCommand = require('./get_astronomy_picture_of_the_day')(config.RANDOM_ID, bot);
 
   scheduler.scheduleJob('30 9 * * *', goodMorningGreetingCommand);
+  scheduler.scheduleJob('31 9 * * *', getMotivationalMessageCommand);
   scheduler.scheduleJob('30 23 * * *', goodNightGreetingCommand);
   scheduler.scheduleJob('0 13 * * *', getAstronomyPictureOfTheDayCommand);
 }
