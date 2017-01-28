1446a7b586a27aacf51431a11081604053acc0f5
Salis Braimah <msbrime@gmail.com>
2017-01-28 15:23:31 +0000

    added joke command
    added a joke command that tells random jokes with Chuck Norris as the main character unless a slack user is mentioned, then that user's name is used instead

--- commands/index.js
+++ commands/index.js
@@ -7,6 +7,7 @@ const {
   urbanDefinitionCommand
 } = require('./definition');
 const morningConvoCommand = require('./morning_conversation');
+const jokeCommand = require('./joke');
 
 module.exports = (controller) => {
 
@@ -18,34 +19,40 @@ module.exports = (controller) => {
     morningConvoCommand
   );
 
-  // Forex Conversion command
-  // Great Britain Pound
+  // // Forex Conversion command
+  // // Great Britain Pound
   controller.hears(
     ['(?=.)^gbp \?(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+)?(\.[0-9]{1,2})?$'], ['direct_message', 'direct_mention', 'mention'],
     forexConversionCommand('gbp', '£')
   );
 
-  // US Dollar
+  // // US Dollar
   controller.hears(
     ['(?=.)^usd \?(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+)?(\.[0-9]{1,2})?$'], ['direct_message', 'direct_mention', 'mention'],
     forexConversionCommand('usd', '$')
   );
 
-  // Euro
+  // // Euro
   controller.hears(
     ['(?=.)^eur \?(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+)?(\.[0-9]{1,2})?$'], ['direct_message', 'direct_mention', 'mention'],
     forexConversionCommand('eur', '€')
   );
 
-  // Definition command
+  // // Definition command
   controller.hears(
     ['real-define', 'what\'s the meaning of', 'whats the meaning of'], ['direct_message', 'direct_mention', 'mention'],
     definitionCommand
   );
-  
-  // Urban Definition command
+
+  // // Urban Definition command
   controller.hears(
     ['define', 'what\'s the urban meaning of', 'whats the urban meaning of'], ['direct_message', 'direct_mention', 'mention'],
     urbanDefinitionCommand
   );
-};
+
+  // Tell Joke Command
+  controller.hears(
+    ['joke', 'lighten\s+the\s+mood', 'laugh'], ['direct_message', 'direct_mention', 'mention'],
+    jokeCommand
+  );
+}
---
+++ commands/joke.js
@@ -0,0 +1,131 @@
+"use strict";
+
+const
+    request = require('request'),
+    randomUA = require('random-ua'),
+    config = require('../config');
+const { capitalizeFirstLetter } = require('../helpers');
+
+/**
+ * Returns the default request headers
+ * @return {Object} 
+ */
+function getRequestHeaders() {
+    return {
+        headers: {
+            'User-Agent': randomUA.generate()
+        }
+    }
+}
+
+/**
+ * Get slack IDs of users mentioned in a message
+ * @param  {String} input
+ * @return {Array}     
+ */
+function getMentionedUsers(input) {
+    var pattern = /<@([\d\w]+)>/ig;
+    var matches = [];
+    var match = pattern.exec(input);
+    while (match) {
+        matches.push(match[1]);
+        match = pattern.exec(input);
+    }
+
+    return matches;
+}
+
+/**
+ * Post a joke
+ * @param  {Object} message 
+ * @param  {Object} bot     
+ * @param  {String} joke   
+ */
+function postJoke(message, bot, joke) {
+    bot.startTyping(message);
+    bot.reply(message, joke);
+}
+
+/**
+ * Post a human friendly error message
+ * @param  {Object} message 
+ * @param  {Object} bot     
+ * @param  {String} error   
+ */
+function postErrorMessage(message, bot, error) {
+    bot.startTyping(message);
+    bot.reply(message, error);
+}
+
+/**
+ * Get a random joke from the ICNDB
+ * @param  {Object} character 
+ * @return {Promise} 
+ */
+function fetchJoke(character) {
+    let url = `${config.ICNDB}?firstName=${character.firstname}&lastName=${character.lastname}`;
+    return new Promise(
+        (resolve, reject) => {
+            request(url, getRequestHeaders(),
+                (error, response, html) => {
+                    if (!error && response.statusCode == 200) {
+                        resolve(JSON.parse(html).value.joke);
+                    } else {
+                        reject(error || false);
+                    }
+                }); // request
+        }); // Promise
+}
+
+/**
+ * Tell a joke with any mentioned user as the main character
+ * @param  {Object} message 
+ * @param  {Object} bot     
+ * @param  {String} user     
+ */
+function tellJokeWithUser(message, bot, user) {
+    bot.api.users.info({ user: user }, (error, response) => {
+        if (!error) {
+            let fetchedUser = response.user;
+            let firstname = fetchedUser.first_name || fetchedUser.name;
+            let lastname = fetchedUser.lastname || "";
+            let userDetails = {
+                firstname: capitalizeFirstLetter(firstname),
+                lastname: capitalizeFirstLetter(lastname)
+            };
+            tellJoke(message, bot, userDetails);
+        }
+        else {
+            postErrorMessage(message, bot, "I dont know who you're talking about");
+        }
+    });
+}
+
+/**
+ * Tell a joke
+ * @param  {Object} message 
+ * @param  {Object} bot     
+ * @param  {String} user     
+ */
+function tellJoke(message, bot, user) {
+    user = Object.assign({ firstname: "Chuck", lastname: "Norris" }, user);
+    fetchJoke(user)
+        .then(joke => {
+            postJoke(message, bot, joke);
+        })
+        .catch(error => {
+            postErrorMessage(message, bot, "I got nothing.Guess the joke's on you");
+        });
+}
+
+module.exports = (bot, message) => {
+    let mentions = getMentionedUsers(message.match.input);
+    if (mentions.length > 0) {
+        mentions.forEach((user) => {
+            tellJokeWithUser(message, bot, user)
+        });
+    }
+    else {
+        tellJoke(message, bot);
+    }
+};
